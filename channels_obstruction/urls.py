e13bca3b233c7b3c6607b76377e3134c56a1f3f1
root <pranavsingh321>
2018-01-08 13:06:41 +0000

    fix  the views and base html

--- channels_obstruction/urls.py
+++ channels_obstruction/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from game.views import *
+from django.contrib.auth.views import login,logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^register/', CreateUserView.as_View()),
+    url(r'^register/', CreateUserView.as_view()),
     url(r'^login/', login, {'template_name':'login.html'}),
     url(r'^logout/', logout, {'next_page':'/'}),
     url(r'^$', HomeView.as_view()),
